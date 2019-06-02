187c92bb3f0df6d79ed29bd89c76fec6cc3de4ab
Odysseus <arwaterman@gmail.com>
2019-06-02 12:14:07 -0700

    added react-router into project

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route} from 'react-router-dom';
+
+const Header = () => <h2>get a Header</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
 
 class App extends Component {
     state = {  }
     render() { 
         return (
             <section>
-                <h1>Omae wa moe shinderu...</h1>
+                <BrowserRouter>
+                    <section>
+                        <Route exact path="/" component={Landing} />
+                        <Route path="/surveys" component={Dashboard} />
+                        <Route path="/surveynew" component={SurveyNew} />
+                    </section>
+                </BrowserRouter>
             </section>
         );
     }
