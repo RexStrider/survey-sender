64258672a05b73b56962a2ed496e811cb851b457
Odysseus <arwaterman@gmail.com>
2019-06-01 17:08:48 -0700

    removed default create-react-app front end files

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    state = {  }
+    render() { 
+        return (
+            <section>
+                <h1>Omae wa moe shinderu...</h1>
+            </section>
+        );
+    }
+}
+ 
+export default App;
