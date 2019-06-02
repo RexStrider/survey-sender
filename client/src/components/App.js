1e076601847f11eca77e4efd2f78cceb4b11a3a2
Odysseus <arwaterman@gmail.com>
2019-06-02 12:17:40 -0700

    updated App component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Route} from 'react-router-dom';
 
 const Header = () => <h2>get a Header</h2>
@@ -6,21 +6,18 @@ const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
-class App extends Component {
-    state = {  }
-    render() { 
-        return (
+const App = () => {
+    return(
             <section>
                 <BrowserRouter>
                     <section>
                         <Route exact path="/" component={Landing} />
-                        <Route path="/surveys" component={Dashboard} />
-                        <Route path="/surveynew" component={SurveyNew} />
+                        <Route exact path="/surveys" component={Dashboard} />
+                        <Route path="/surveys/new" component={SurveyNew} />
                     </section>
                 </BrowserRouter>
             </section>
-        );
-    }
+    )
 }
  
 export default App;
