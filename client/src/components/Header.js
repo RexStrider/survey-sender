d22d4a844bf9824cac04f262c25f80100f1658c9
Odysseus <arwaterman@gmail.com>
2019-06-11 18:57:44 -0700

    implementing login/logout button in header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Header extends Component {
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return 'resolving request...';
+            case false:
+                return <li><a href="/auth/google">Login With Google</a></li>;
+            default:
+                return <li><a href="/api/logout">Logout</a></li>;
+        }
+    }
+
     render() { 
         console.log(this.props);
         return (
@@ -12,7 +23,7 @@ class Header extends Component {
                     </a>
                     <ul className='right'>
                         <li>
-                            <a>Login with Google</a>
+                            {this.renderContent()}
                         </li>
                     </ul>
                 </section>
