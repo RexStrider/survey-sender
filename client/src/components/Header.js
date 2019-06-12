bad2d6c4c2fbf73f2a5bbad951756ea04f09f247
Odysseus <arwaterman@gmail.com>
2019-06-11 20:24:12 -0700

    updated routing behavior of application

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
     renderContent() {
@@ -18,9 +19,12 @@ class Header extends Component {
         return (
             <nav>
                 <section className='nav-wrapper'>
-                    <a className='left brand-logo'>
+                    <Link
+                        to={this.props.auth ? '/surveys' : '/'}
+                        className='left brand-logo'
+                    >
                         Survey Sender
-                    </a>
+                    </Link>
                     <ul className='right'>
                         <li>
                             {this.renderContent()}
---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <section style = {{ textAlign: 'center' }}>
+            <h1>
+                Survey Sender
+            </h1>
+            <p>Collect feedback from your users!</p>
+        </section>
+    );
+};
+
+export default Landing;
