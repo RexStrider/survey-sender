3f109a26ccfcaa89cc24949d1b6f63bc06bdb9ad
Odysseus <arwaterman@gmail.com>
2019-07-02 22:11:16 -0700

    added Payments component

---
+++ client/src/components/Payments.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+class Payments extends Component {
+    state = {  }
+
+    render() { 
+        return (
+            <StripeCheckout
+                // 5 dollars or 500 cents
+                amount={5 * 100}
+                token={token => console.log(token)}
+                stripeKey={process.env.REAC_APP_STRIPE_KEY}
+            />
+        );
+    }
+}
+ 
+export default Payments;
