556758f8ba2c0012e5efba73e71ad9d781782ce1
Odysseus <arwaterman@gmail.com>
2019-07-05 19:51:21 -0700

    updating application"

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const cookieSession = require('cookie-session');
+const bodyParser = require('body-parser');
 
 const keys = require('./config/keys');
 
@@ -13,6 +14,7 @@ mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 1000,
