dd9ac38c59e8a13d5746bc91b746f0ea6775ffca
Odysseus <arwaterman@gmail.com>
2019-10-26 20:42:49 -0700

    updating survey route and mailer

--- routes/survey.js
+++ routes/survey.js
@@ -7,7 +7,7 @@ const template = require("../services/emailTemplates/survey");
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-    app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
         const { title, subject, body, recipients } = req.body;
 
         const survey = new Survey({
@@ -21,6 +21,15 @@ module.exports = app => {
 
         // Send an email
         const mailer = new Mailer(survey, template(survey));
-        mailer.send();
+        try {
+            await mailer.send();
+            await survey.save();
+            req.user.credits -= 1;
+            const user = await req.user.save();
+            res.send(user);
+        }
+        catch (err) {
+            res.status(422).send(err);
+        }
     });
 };
