99a3d5412f5d9614382f736978d332ce3d0e93d6
Odysseus <arwaterman@gmail.com>
2019-11-01 14:46:28 -0700

    added field component to survey form component

--- client/src/components/Surveys/SurveyForm.js
+++ client/src/components/Surveys/SurveyForm.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
 
 class SurveyForm extends Component {
     render() { 
         return (
             <section>
                 <h2>Survey Form</h2>
+                <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+                    <Field type="text" name="surveyTitle" component="input" />
+                    <button type="submit">Submit</button>
+                </form>
             </section>
         );
     }
 }
  
 export default reduxForm({
-    from: 'surveyForm'
+    form: 'surveyForm'
 })(SurveyForm);
