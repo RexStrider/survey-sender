dbacc112e7ba09f82576e9b3f6cc443c51853d87
Odysseus <arwaterman@gmail.com>
2019-11-01 16:35:41 -0700

    added cancel button to the survey form component

--- client/src/components/Surveys/SurveyForm.js
+++ client/src/components/Surveys/SurveyForm.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import SurveyField from './SurveyField';
+import { Link } from 'react-router-dom';
 
 const FIELDS = [
     {label: "Survey Title", name: "title"},
@@ -28,7 +29,11 @@ class SurveyForm extends Component {
             <section>
                 <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
                     {this.renderFields()}
-                    <button type="submit">Submit</button>
+                    <Link to="/surveys" type="submit" className="red btn-flat white-text">Cancel</Link>
+                    <button type="submit" className="teal btn-flat right white-text">
+                        Next
+                        <i className="material-icons right">done</i>
+                    </button>
                 </form>
             </section>
         );
