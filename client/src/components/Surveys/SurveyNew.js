9981c730cbe8cf465bac3eafeaaebcbac511fe3b
Odysseus <arwaterman@gmail.com>
2019-10-31 17:48:42 -0700

    added redux-form package and survey new component

---
+++ client/src/components/Surveys/SurveyNew.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SurveyNew extends Component {
+    state = {  }
+    render() { 
+        return (
+            <section>
+                <h2>SurveyNew</h2>
+            </section>
+        );
+    }
+}
+ 
+export default SurveyNew;
