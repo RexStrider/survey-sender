f93e49ac6ebb855cd745f5e7034829564abf9216
Odysseus <arwaterman@gmail.com>
2019-11-03 19:30:58 -0800

    refactored/renamed emails property to recipients property

--- client/src/components/Surveys/formFields.js
+++ client/src/components/Surveys/formFields.js
@@ -2,5 +2,5 @@ export default [
     {label: "Survey Title", name: "title", noValueError: "provide a survey name"},
     {label: "Subject Line", name: "subject", noValueError: "provide a subject line"},
     {label: "Email Body", name: "body", noValueError: "provide an email body"},
-    {label: "Recipient List", name: "emails", noValueError: "provide one or more email addresses"}
+    {label: "Recipient List", name: "recipients", noValueError: "provide one or more email addresses"}
 ];
