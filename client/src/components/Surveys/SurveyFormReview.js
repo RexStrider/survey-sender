f497abdeaad8d9e70fa4464b2b029e56f3807845
Odysseus <arwaterman@gmail.com>
2019-11-03 19:52:23 -0800

    sending survey navigates user back tot he dashboard

--- client/src/components/Surveys/SurveyFormReview.js
+++ client/src/components/Surveys/SurveyFormReview.js
@@ -2,9 +2,10 @@ import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
+import { withRouter } from 'react-router-dom';
 import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
     const reviewFields = _.map(formFields, ({ label, name }) => {
         return (
             <section key={name}>
@@ -21,7 +22,7 @@ const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
             <button className='yellow darken-3 btn-flat' onClick={onCancel} >
                 Back
             </button>
-            <button className='green white-text btn-flat right' onClick={() => submitSurvey(formValues)}>
+            <button className='green white-text btn-flat right' onClick={() => submitSurvey(formValues, history)}>
                 Send Survey
                 <i className="material-icons right">email</i>
             </button>
@@ -33,4 +34,4 @@ function mapStateToProps(state) {
     return{ formValues: state.form.surveyForm.values };
 }
 
-export default connect(mapStateToProps, actions)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
