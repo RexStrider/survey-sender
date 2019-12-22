08bb7c3f2a3e5f1fa671f3ba118868d94948ef8d
Odysseus <arwaterman@gmail.com>
2019-12-22 00:39:23 -0800

    added SurveyList component

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import SurveyList from './Surveys/SurveyList';
 
 const Dashboard = () => {
     return (
         <section>
-            <h2>Dashboard</h2>
+            <SurveyList />
             <section className="fixed-action-btn">
                 <Link to="/surveys/new" className="btn-floating btn-large red">
                     <i className="material-icons">add</i>
